fb69f8fe49b4e75fc4bbb6c1d69f3bc43e16591c
Frank Tran <frank.tran@live.ca>
2021-10-19 22:39:26 -0400

    adding serverside cache

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const stock = require('./controllers/stock');
+const apicache = require('apicache');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// MIDDLEWARE
+let cache = apicache.middleware;
+const onlyStatus200 = (req, res) => res.statusCode === 200;
+const cacheSuccesses = cache('15 minutes', onlyStatus200);
 
 // HOME
 app.get('/', (req, res) => {
@@ -15,10 +21,10 @@ app.get('/', (req, res) => {
 app.get('/search/:ticker', (req, res) => {
   stock.getCompanyOverview(req, res);
 });
-app.get('/chart/intraday/:ticker', (req, res) => {
+app.get('/chart/intraday/:ticker', cacheSuccesses, (req, res) => {
   stock.getIntradayData(req, res);
 });
-app.get('/chart/daily/:ticker', (req, res) => {
+app.get('/chart/daily/:ticker', cacheSuccesses, (req, res) => {
   stock.getDailyData(req, res);
 });
 
@@ -26,4 +32,4 @@ let port = process.env.PORT || 3000;
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log('RESTful API server started on: ' + port);
