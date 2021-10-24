bf7eed8bb947ff14fd6f0ab40ffb879b351869aa
Frank Tran <frank.tran@live.ca>
2021-10-23 22:32:54 -0400

    Include insiders and latest news api

---
+++ controllers/misc.js
@@ -0,0 +1,45 @@
+'use strict';
+const request = require('request');
+require('dotenv').config();
+
+const getLatestNews = (req, res) => {
+  let url = `https://financialmodelingprep.com/api/v3/stock_news?limit=100&apikey=
+    ${process.env.FMP_API_KEY}`;
+
+  request.get(
+    {
+      url: url,
+      json: true,
+      headers: { 'User-Agent': 'request' },
+    },
+    (err, data) => {
+      if (err) {
+        res.statusCode(404).json('Could Not Retrieve News');
+      } else {
+        // data is successfully parsed as a JSON object:
+        res.json(data);
+      }
+    }
+  );
+};
+
+const getInsiders = (req, res) => {
+  let url = `https://financialmodelingprep.com/api/v4/insider-trading?transactionType=P-Purchase,S-Sale&limit=100&apikey=
+    ${process.env.FMP_API_KEY}`;
+
+  request.get(
+    {
+      url: url,
+      json: true,
+      headers: { 'User-Agent': 'request' },
+    },
+    (err, data) => {
+      if (err) {
+        res.statusCode(404).json('Could Not Retrieve Insiders');
+      } else {
+        // data is successfully parsed as a JSON object:
+        res.json(data);
+      }
+    }
+  );
+};
